830917b9c06955599ab2c419987a6aee7beac95e
lbartron <LeoB909@hotmail.com>
2025-08-15 00:22:42 -0700

    Removed unused function

---
+++ src/MovingObject.hpp
@@ -0,0 +1,73 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+#include <vector>
+
+class MovingObject {
+private:
+    sf::CircleShape shape;
+    sf::Vector2f position;
+    sf::Vector2f lastPosition;
+    sf::Vector2f acceleration;
+    float radius;
+
+public:
+    MovingObject(sf::Vector2f position, float radius){
+        shape.setRadius(radius);
+        shape.setPosition(position);
+        shape.setOrigin(radius, radius);
+
+        this->position = position;
+        this->radius = radius;
+        lastPosition = position;
+        acceleration = {0.0f, 0.0f};
+        shape.setPointCount(8); //optimize for fps
+    }
+
+    void update(float deltaT){
+        //sf::Vector2f newPosition = position + acceleration * deltaT * deltaT;
+        sf::Vector2f displacement = position - lastPosition;
+        lastPosition = position;
+        position += displacement + acceleration * (deltaT * deltaT);
+        shape.setPosition(position);
+    }
+
+    void reflectX(float COR){
+        float displacement = position.x - lastPosition.x;
+        lastPosition.x = position.x + displacement * COR;
+    }
+
+    void reflectY(float COR){
+        float displacement = position.y - lastPosition.y;
+        lastPosition.y = position.y + displacement * COR;
+    }
+
+    void setVelocity(sf::Vector2f newVelocity, float dt){
+        lastPosition = position - (newVelocity * dt);
+    }
+    
+
+    void addAcceleration(sf::Vector2f newAcceleration){
+        acceleration += newAcceleration;
+    }
+
+    void resetAcceleration(){
+        acceleration = {0.0f, 0.0f};
+    }
+
+    void setPosition(sf::Vector2f newPosition){
+        position = newPosition;
+        shape.setPosition(newPosition);
+    }
+
+    sf::CircleShape& getShape(){
+        return shape;
+    }
+
+    sf::Vector2f getPosition(){
+        return position;
+    }
+
+    float getRadius(){
+        return radius;
+    }
+};
